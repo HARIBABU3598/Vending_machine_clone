ae05d3e718dc17498c49f45c995211b9c958245c
HARIBABU3598 <harirom5444v@gmail.com>
2025-02-08 08:52:52 +0530

    first-m

---
+++ script.js
@@ -0,0 +1,273 @@
+var cnt=0
+var cnt2=0
+var cnt3=0
+var cnt4=0
+var cnt5=0
+var cnt6=0
+var cnt7=0
+var cnt8=0
+var cnt9=0
+var cnt10=0
+var cnt11=0
+var cnt12=0
+var cnt13=0
+var cnt14=0
+var cnt15=0
+var cnt16=0
+var cnt17=0
+var cnt18=0
+var cnt19=0
+var cnt20=0
+var cnt21=0
+var cnt22=0
+var cnt23=0
+var cnt24=0
+var one=document.getElementById("lays")
+var two=document.getElementById("kurkure")
+var three=document.getElementById("bingo")
+var four=document.getElementById("veg")
+var five=document.getElementById("mad")
+var six=document.getElementById("cheetos")
+var seven=document.getElementById("miranda")
+var eight=document.getElementById("sprite")
+var nine=document.getElementById("7up")
+var ten=document.getElementById("coke")
+var eleven=document.getElementById("dew")
+var twelve=document.getElementById("fizz")
+var thirteen=document.getElementById("diary")
+var fourteen=document.getElementById("kit")
+var fifteen=document.getElementById("milky")
+var sixteen=document.getElementById("5star")
+var seventeen=document.getElementById("munch")
+var eighteen=document.getElementById("snickers")
+var nineteen=document.getElementById("milk")
+var twenty=document.getElementById("tiger")
+var twentyone=document.getElementById("jim")
+var twentytwo=document.getElementById("marie")
+var twentythree=document.getElementById("good")
+var twentyfour=document.getElementById("ff")
+
+// 1st item
+function add(){
+    one.textContent=cnt+1
+    cnt=cnt+1
+}
+function sub(){
+    one.textContent=cnt-1
+    cnt=cnt-1
+}
+
+// 2nd item
+function add2(){
+    two.textContent=cnt2+1
+    cnt2=cnt2+1
+}
+function sub2(){
+    two.textContent=cnt2-1
+    cnt2=cnt2-1
+}
+
+// 3rd item
+function add3(){
+    three.textContent=cnt3+1
+    cnt3=cnt3+1
+}
+function sub3(){
+    three.textContent=cnt3-1
+    cnt3=cnt3-1
+}
+
+function add4(){
+    four.textContent=cnt4+1
+    cnt4=cnt4+1
+}
+function sub4(){
+    four.textContent=cnt4-1
+    cnt4=cnt4-1
+}
+
+function add5(){
+    five.textContent=cnt5+1
+    cnt5=cnt5+1
+}
+function sub5(){
+    five.textContent=cnt5-1
+    cnt5=cnt5-1
+}
+
+function add6(){
+    six.textContent=cnt6+1
+    cnt6=cnt6+1
+}
+function sub6(){
+    six.textContent=cnt6-1
+    cnt6=cnt6-1
+}
+
+function add7(){
+    seven.textContent=cnt7+1
+    cnt7=cnt7+1
+}
+function sub7(){
+    seven.textContent=cnt7-1
+    cnt7=cnt7-1
+}
+
+function add8(){
+    eight.textContent=cnt8+1
+    cnt8=cnt8+1
+}
+function sub8(){
+    eight.textContent=cnt8-1
+    cnt8=cnt8-1
+}
+
+function add9(){
+    nine.textContent=cnt9+1
+    cnt9=cnt9+1
+}
+function sub9(){
+    nine.textContent=cnt9-1
+    cnt9=cnt9-1
+}
+
+function add10(){
+    ten.textContent=cnt10+1
+    cnt10=cnt10+1
+}
+function sub10(){
+    ten.textContent=cnt10-1
+    cnt10=cnt10-1
+}
+
+function add11(){
+    eleven.textContent=cnt11+1
+    cnt11=cnt11+1
+}
+function sub11(){
+    eleven.textContent=cnt11-1
+    cnt11=cnt11-1
+}
+
+function add12(){
+    twelve.textContent=cnt12+1
+    cnt12=cnt12+1
+}
+function sub12(){
+    twelve.textContent=cnt12-1
+    cnt12=cnt12-1
+}
+
+function add13(){
+    thirteen.textContent=cnt13+1
+    cnt13=cnt13+1
+}
+function sub13(){
+    thirteen.textContent=cnt13-1
+    cnt13=cnt13-1
+}
+
+function add14(){
+    fourteen.textContent=cnt14+1
+    cnt14=cnt14+1
+}
+function sub14(){
+    fourteen.textContent=cnt14-1
+    cnt14=cnt14-1
+}
+
+function add15(){
+    fifteen.textContent=cnt15+1
+    cnt15=cnt15+1
+}
+function sub15(){
+    fifteen.textContent=cnt15-1
+    cnt15=cnt15-1
+    }
+
+function add16(){
+    sixteen.textContent=cnt16+1
+    cnt16=cnt16+1
+}
+function sub16(){
+    sixteen.textContent=cnt16-1
+    cnt16=cnt16-1
+}
+
+function add17(){
+    seventeen.textContent=cnt17+1
+    cnt17=cnt17+1
+}
+function sub17(){
+    seventeen.textContent=cnt17-1
+    cnt17=cnt17-1
+}
+
+function add18(){
+    eighteen.textContent=cnt18+1
+    cnt18=cnt18+1
+}
+function sub18(){
+    eighteen.textContent=cnt18-1
+    cnt18=cnt18-1
+}
+
+function add19(){
+    nineteen.textContent=cnt19+1
+    cnt19=cnt19+1
+}
+function sub19(){
+    nineteen.textContent=cnt19-1
+    cnt19=cnt19-1
+}
+
+function add20(){
+    twenty.textContent=cnt20+1
+    cnt20=cnt20+1
+}
+function sub20(){
+    twenty.textContent=cnt20-1
+    cnt20=cnt20-1
+}
+
+function add21(){
+    twentyone.textContent=cnt21+1
+    cnt21=cnt21+1
+}
+function sub21(){
+    twentyone.textContent=cnt21-1
+    cnt21=cnt21-1
+}
+
+function add22(){
+    twentytwo.textContent=cnt22+1
+    cnt22=cnt22+1
+}
+function sub22(){
+    twentytwo.textContent=cnt22-1
+    cnt22=cnt22-1
+}
+
+function add23(){
+    twentythree.textContent=cnt23+1
+    cnt23=cnt23+1
+}
+function sub23(){
+    twentythree.textContent=cnt23-1
+    cnt23=cnt23-1
+}
+
+function add24(){
+    twentyfour.textContent=cnt24+1
+    cnt24=cnt24+1
+}
+function sub24(){
+    twentyfour.textContent=cnt24-1
+    cnt24=cnt24-1
+}
+var quan=document.getElementById("quan")
+var amount=document.getElementById("amount")
+function check(){
+    quan.textContent=cnt+cnt2+cnt3+cnt4+cnt5+cnt6+cnt7+cnt8+cnt9+cnt10+cnt11+cnt12+cnt13+cnt14+cnt15+cnt16+cnt17+cnt18+cnt19+cnt20+cnt21+cnt22+cnt23+cnt24
+    amount.textContent=cnt*10+cnt2*10+cnt3*10+cnt4*10+cnt5*10+cnt6*10+cnt7*40+cnt8*40+cnt9*40+cnt10*40+cnt11*40+cnt12*40+cnt13*10+cnt14*20+cnt15*30+cnt16*5+cnt17*10+cnt18*10+cnt19*10+cnt20*10+cnt21*10+cnt22*10+cnt23*10+cnt24*10
+}
